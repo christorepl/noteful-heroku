6244c5d3f888482bde56acf2e8f6dbbb830668d8
Christopher <chrisobrien416@gmail.com>
2020-11-11 19:04:04 -0500

    done i hope

---
+++ postgrator-config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+module.exports = {
+    'migrationDirectory': 'migrations',
+    'driver': 'pg',
+    'connectionString': `postgres://xtottmugrfawpy:eaaf9f2f3002a79ac4d362b4042c3e43524a07277dfa2ea17271a740df78124c@ec2-100-25-100-81.compute-1.amazonaws.com:5432/dvh84sqkjue4o`
+};
